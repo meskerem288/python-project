7e6a0a2b6a024a005b519d5e9675d0d42aebcc47
meskerem288 <meskihailu2@gmail.com>
2025-06-13 19:53:09 -0400

    task to do list

---
+++ to_do_list_app.py
@@ -0,0 +1,7 @@
+# 3. To-Do List App (Text-Based)
+# Build a to-do list manager that:
+# Allows users to add tasks with priorities (e.g., "Buy milk - high").
+# Lets them view the current list, delete tasks by number, and mark tasks as complete.
+# Keeps looping until the user types “exit”.
+# Shows a summary at the end: number of completed tasks vs pending.
+# Skills practiced: lists, string parsing, loops, input, CRUD basics
