34e18c483828d65622ab9ba84852b6aa6c6f09e6
meskerem288 <meskihailu2@gmail.com>
2025-06-13 19:46:57 -0400

    simple checkout

---
+++ simple_grocery_checkout.py
@@ -0,0 +1,9 @@
+# Simple Grocery Cart Checkout
+# Write a program that:
+# Has a predefined dictionary of groceries with prices.
+# Lets the user “add” items by typing their names.
+# For each valid item, asks for the quantity.
+# Keeps adding to the cart until the user types "checkout".
+# Displays a final bill: each item, quantity, subtotal, and total.
+# Skills practiced: dictionaries, loops, input(), math operations, formatting, error handling
+//
