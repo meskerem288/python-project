8a8e02612cb9ca9cfba296580196a6651ea164b8
meskerem288 <meskihailu2@gmail.com>
2025-06-13 15:14:58 -0400

    Vending machine program G 3

--- vending_machine.py
+++ vending_machine.py
@@ -1,5 +1,10 @@
-Total cost
-Total cost
-Total cost
-Total cost
-Total cost
+# 1. Vending Machine Program
+# Build a program that:
+# Displays a list of snacks and drinks with item numbers and prices.
+# Asks the user to choose items by number in a loop.
+# Keeps track of selected items and their prices.
+# Ends when the user types “done”.
+# Finally prints a receipt showing:
+# List of selected items with prices
+# Total cost
+# Skills practiced: loops, input(), conditionals, lists/dictionaries, sum(), print formatting
