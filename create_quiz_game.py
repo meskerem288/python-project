9f86f6b3a4ebcd01e3eee6c61e70dbb3a69fa264
meskerem288 <meskihailu2@gmail.com>
2025-06-13 20:04:21 -0400

    list_of_questions

---
+++ create_quiz_game.py
@@ -0,0 +1,8 @@
+# 5. Quiz Game
+# Create a basic quiz game that:
+# Contains a list of 5–10 questions stored in a dictionary (or list of dicts).
+# Asks the user each question and records their answers.
+# At the end, displays:
+# The user’s score (e.g., 7/10)
+# Correct answers for any questions they got wrong
+# Skills practiced: loops, dictionaries, input, comparison, counters, print formatting
