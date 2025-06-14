4b828c733a1bc63db3de2fe0161830b9f77538dd
meskerem288 <meskihailu2@gmail.com>
2025-06-13 20:00:15 -0400

    ticket_booking

---
+++ movie_ticket_booking_simulation.py
@@ -0,0 +1,7 @@
+#  4.Movie Ticket Booking Simulation
+# Simulate a movie theater booking system that:
+# Shows a list of available movie titles, showtimes, and seat prices.
+# Asks the user to choose a movie and number of tickets.
+# Confirms total price and asks if they want to book another movie.
+# Ends when they say “no” and displays total bookings and cost.
+# Skills practiced: loops, input, conditionals, calculations, nested dictionaries
