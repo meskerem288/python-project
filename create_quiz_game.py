da8e6830547242b0931e06921168cfb148a13744
meskerem288 <meskihailu2@gmail.com>
2025-06-23 23:18:10 -0400

    update

--- create_quiz_game.py
+++ create_quiz_game.py
@@ -1,8 +1,29 @@
-# 5. Quiz Game
 # Create a basic quiz game that:
 # Contains a list of 5–10 questions stored in a dictionary (or list of dicts).
 # Asks the user each question and records their answers.
 # At the end, displays:
 # The user’s score (e.g., 7/10)
 # Correct answers for any questions they got wrong
-# Skills practiced: loops, dictionaries, input, comparison, counters, print formatting
+print("welcome to quiz game!!!")
+dic={"question1":"answer1",
+     "question2":"answer2",
+     "question3":"answer3",
+     "question4":"answer4",
+     "question5":"answer5",
+     "question6":"answer6",
+     "question7":"answer7",
+     "question8":"answer8",
+     "question9":"answer9",
+     "question10":"answer10"}
+print("please answer the following question")
+score=0
+for key in dic:
+    ans = input(f"what is the correct answer for {key} ")
+    if ans == dic.get(key):
+        print("correct")
+        score+=1
+    else:
+        print("the correct answer is :" ,dic.get(key))
+print(f"you have completed the quiz , you scored : {score}/10")
+
+
--- movie_ticket_booking_simulation.py
+++ movie_ticket_booking_simulation.py
@@ -5,3 +5,32 @@
 # Confirms total price and asks if they want to book another movie.
 # Ends when they say “no” and displays total bookings and cost.
 # Skills practiced: loops, input, conditionals, calculations, nested dictionaries
+#Movie Title Mickey Mouse Show Time 10pm
+titles= ["Mickey Mouse","Dora","Paw patrol"]
+booking ={"10pm":14,
+          "1pm" : 35,
+          "2pm" : 45}
+
+print("here are the available movies with their show times and seat prices!!")
+index=0
+booked={}
+for time in booking:
+    print(f"Movie Title: {titles[index]} Show Time: {time} Price: {booking.get(time)}$")
+    index+=1
+while True:
+    title = input("pick a movie list e.g Mickey Mouse ")
+    if title=="no":
+        break
+    if title not in titles:
+        print("please enter available movie in the list only")
+        continue
+    num_ticket = int(input("enter the number of ticket you want to buy "))
+    print("please type no when you are done!")
+    ind = titles.index(title)
+    show_time = list(booking.keys())[ind]
+    price_per_ticket = booking[show_time]
+    total_price = num_ticket * price_per_ticket
+    booked[title]=total_price
+print(f"The booked movie list : , {booked}")
+
+
